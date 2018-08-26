6ef637f189798bf28a3a97d5042c0179bf86aa41
sproulel <sproulelucas13@gmail.com>
2018-08-25 23:42:59 -0400

    Add files via upload

---
+++ mandelbrot.py
@@ -0,0 +1,37 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def mandelbrot_set(xmin, xmax, ymin, ymax, xn, yn, maxiter, horizon=2.0):
+    X = np.linspace(xmin, xmax, xn, dtype=np.float32)
+    Y = np.linspace(ymin, ymax, yn, dtype=np.float32)
+    C = X + Y[:, None]*1j
+    N = np.zeros(C.shape, dtype=int)
+    Z = np.zeros(C.shape, np.complex64)
+    for n in range(maxiter):
+        I = np.less(abs(Z), horizon)
+        N[I] = n
+        Z[I] = Z[I]**2 + C[I]
+    N[N == maxiter-1] = 0
+    return Z, N
+
+
+if __name__ == '__main__':
+    xmin, xmax, xn = -2.25, +0.75, 3000/2
+    ymin, ymax, yn = -1.25, +1.25, 2500/2
+    iterations = 200
+    horizon = 2.0 ** 40
+    log = np.log(np.log(horizon))/np.log(2)
+    Z, N = mandelbrot_set(xmin, xmax, ymin, ymax, xn, yn, iterations, horizon)
+
+    with np.errstate(invalid='ignore'):
+        M = np.nan_to_num(N + 1 -np.log(np.log(abs(Z)))/np.log(2) + log)
+
+    dpi = 72
+    width = 10
+    height = 10*yn/xn
+    fig = plt.figure(figsize=(width, height), dpi=dpi)
+    ax = fig.add_axes([0.0, 0.0, 1.0, 1.0], frameon=False, aspect=1)
+    plt.imshow(M, extent=[xmin, xmax, ymin, ymax], interpolation="bicubic")
+    ax.set_xticks([])
+    ax.set_yticks([])
+    plt.show()
